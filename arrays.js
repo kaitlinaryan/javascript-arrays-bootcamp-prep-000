3a4a9d28c914e650db592295f50462ed9e71714d
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2017-12-20 22:31:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,7 @@ function removeElementFromBeginningOfArray(array) {
   array.slice(1)
   return array
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop(1)
+  return array
+}
